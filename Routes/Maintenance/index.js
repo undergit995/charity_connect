f92cd079f36c58546c41a62aef5771777d9ee5a1
undergit995 <avinash.avi7a@gmail.com>
2026-07-17 02:19:42 +0530

    env added

--- Routes/Maintenance/index.js
+++ Routes/Maintenance/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({ 
-    maintenance: process.env.MAINTENANCE_MODE === 'true' || false,
+    maintenance: false,
     message: 'We are currently under maintenance. Please check back later.'
   });
 });
